25af0e157092394f5501bb7ec45aea464ef73bea
Luis Fernando Choque <luisfer_2810@outlook.com>
2022-01-12 16:14:13 -0400

    AB#78 Logout reducer call done

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,22 @@ import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Router from "./components/Router";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useHistory } from "react-router-dom";
 import LayoutWrapper from "./components/Layoutwrapper";
 import { useIdleTimer } from "react-idle-timer";
+import { useDispatch } from "react-redux";
+import { LogOut } from "./redux/authSlice";
 
 function App() {
-	const handleOnIdle = (event) => {
-		console.log("user is idle", event);
-		console.log("last active", getLastActiveTime());
-	};
-
-	const handleOnActive = (event) => {
-		console.log("user is active", event);
-		console.log("time remaining", getRemainingTime());
-	};
-
-	const handleOnAction = (event) => {
-		console.log("user did something", event);
+	const history = useHistory();
+	const dispatch = useDispatch();
+	const handleOnIdle = () => {
+		dispatch(LogOut());
 	};
 
 	const { getRemainingTime, getLastActiveTime } = useIdleTimer({
-		timeout: 900000, //15 min
+		timeout: 120000,//900000, //15 min
 		onIdle: handleOnIdle,
-		onActive: handleOnActive,
-		onAction: handleOnAction,
 		debounce: 500,
 	});
 
