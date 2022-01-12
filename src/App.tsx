2e34af9f90369724aee755f0f07428889b412b62
Luis Fernando Choque <luisfer_2810@outlook.com>
2022-01-12 16:01:38 -0400

    AB#78 Idle logout in 15 minutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,43 @@
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Router from './components/Router';
-import { BrowserRouter } from 'react-router-dom';
-import LayoutWrapper from './components/Layoutwrapper';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Router from "./components/Router";
+import { BrowserRouter } from "react-router-dom";
+import LayoutWrapper from "./components/Layoutwrapper";
+import { useIdleTimer } from "react-idle-timer";
 
 function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <LayoutWrapper>
-            <Router />
-        </LayoutWrapper>
-      </BrowserRouter>
-    </div>
-  );
+	const handleOnIdle = (event) => {
+		console.log("user is idle", event);
+		console.log("last active", getLastActiveTime());
+	};
+
+	const handleOnActive = (event) => {
+		console.log("user is active", event);
+		console.log("time remaining", getRemainingTime());
+	};
+
+	const handleOnAction = (event) => {
+		console.log("user did something", event);
+	};
+
+	const { getRemainingTime, getLastActiveTime } = useIdleTimer({
+		timeout: 900000, //15 min
+		onIdle: handleOnIdle,
+		onActive: handleOnActive,
+		onAction: handleOnAction,
+		debounce: 500,
+	});
+
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<LayoutWrapper>
+					<Router />
+				</LayoutWrapper>
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
