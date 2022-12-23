e1b854d5a1ab0df277559119fa1943b8c3866eed
Luis Fernando Choque <luisfer_2810@outlook.com>
2022-12-23 19:57:11 -0400

    update on users card

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -60,12 +60,90 @@ function UserCard({
 							alignItems: "center",
 							alignContent: "center",
 							flexDirection: "row",
+							width: "100%",
 						}}
 					>
-						<Typography variant="h5" style={{ fontWeight: 600 }}>
-							Nombre de usuario:
-						</Typography>
-						<Typography variant="h6">{username}</Typography>
+						<Grid container>
+							<Grid item xs={4}>
+								<Typography
+									variant="h6"
+									align="left"
+									style={{ fontWeight: 600 }}
+								>
+									Nombre de usuario:
+								</Typography>
+							</Grid>
+							<Grid
+								item
+								xs={8}
+								sx={{
+									display: "flex",
+									justifyContent: "flex-start",
+									alignItems: "center",
+									alignContent: "center",
+									flexDirection: "row",
+									width: "100%",
+								}}
+							>
+								<Typography align="left" variant="h6">
+									{username}
+								</Typography>
+							</Grid>
+						</Grid>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							alignContent: "center",
+							flexDirection: "row",
+							width: "100%",
+						}}
+					>
+						<Grid container>
+							<Grid item xs={4}>
+								<Typography
+									variant="h6"
+									align="left"
+									style={{ fontWeight: 600 }}
+								>
+									Nombre:
+								</Typography>
+							</Grid>
+							<Grid item xs={8}>
+								<Typography variant="h6" align="left">
+									{firstName}
+								</Typography>
+							</Grid>
+						</Grid>
+					</Box>
+					<Box
+						sx={{
+							display: "flex",
+							justifyContent: "center",
+							alignItems: "center",
+							alignContent: "center",
+							flexDirection: "row",
+							width: "100%",
+						}}
+					>
+						<Grid container>
+							<Grid item xs={4}>
+								<Typography
+									variant="h6"
+									style={{ fontWeight: 600 }}
+									align="left"
+								>
+									Apellido:
+								</Typography>
+							</Grid>
+							<Grid item xs={8}>
+								<Typography align="left" variant="h6">
+									{lastName}
+								</Typography>
+							</Grid>
+						</Grid>
 					</Box>
 					<Box
 						sx={{
@@ -76,36 +154,8 @@ function UserCard({
 							flexDirection: "row",
 						}}
 					>
-						<Typography variant="h5" style={{ fontWeight: 600 }}>
-							Nombre:
-						</Typography>
-						<Typography variant="h6">{firstName}</Typography>
-					</Box>
-					<Box
-						sx={{
-							display: "flex",
-							justifyContent: "center",
-							alignItems: "center",
-							alignContent: "center",
-							flexDirection: "row",
-						}}
-					>
-						<Typography variant="h5" style={{ fontWeight: 600 }}>
-							Apellido:
-						</Typography>
-						<Typography variant="h6">{lastName}</Typography>
-					</Box>
-					<Box
-						sx={{
-							display: "flex",
-							justifyContent: "center",
-							alignItems: "center",
-							alignContent: "center",
-							flexDirection: "row",
-						}}
-					>
-						<Typography variant="h5" style={{ fontWeight: 600 }}>
-							{admin ? "Es admin" : "No es admin"}
+						<Typography variant="h6" style={{ fontWeight: 400 }}>
+							{admin ? "Es administrador" : "No es administrador"}
 						</Typography>
 					</Box>
 				</Grid>
